bd03d6b23ae85bf6afc6f0a8b413eaca94378964
王晓辉 <121578151@qq.com>
2017-11-14 19:02:52 +0800

    no message

--- proxy/post.js
+++ proxy/post.js
@@ -316,12 +316,11 @@ exports.save = function (params, callback) {
             IsActive: true,
             ModifyTime: new Date()
         });
-x
     async.parallel({
         post: function (done) {
             postModel.findById(_id, function (err, article) {
                 if (err) {
-                     done(err);
+                    return done(err);
                 }
                 if (!article) {
                     //新增
@@ -338,7 +337,7 @@ x
                     //更新
                     postModel.update({"_id": _id}, entity, function (err) {
                         if (err) {
-                             done(err);
+                            return done(err);
                         }
                         done()
 
@@ -355,14 +354,14 @@ x
                 .select('CategoryId Alias')
                 .exec(function (err, data) {
                     if (err) {
-                         done(err);
+                        return done(err);
                     }
 
                     data.forEach(function (item) {
 
                         categoryModel.findById(item.CategoryId, function (err, category) {
                             if (err) {
-                                 done(err);
+                                return  done(err);
                             }
                             URLS.push({
                                 url:'/blog/' + category.Alias+'/'+item.Alias,
